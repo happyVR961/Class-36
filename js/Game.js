2fb018a5f23cef04c4743d4dc6107f1af74d86b6
happyVR961 <happyVR961@gmail.com>
2020-08-06 17:50:37 -0700

    class36

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,30 @@ class Game {
       form.display();
     }
   }
+  play(){
+    form.hidden();
+    textSize(30);
+    fill("blue");
+    text("Game Start",120,70);
+    Player.getPlayerInfo();
+    if (allPlayers !== undefined){
+      var displayPosition = 130;
+      for(var plr in allPlayers){
+        if (plr === "player"+player.index){
+          fill("red");
+        }
+        else{
+          fill("black");
+        }
+        displayPosition += 30;
+        textSize(15);
+        text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,displayPosition);
+      }
+
+    }
+    if (keyDown(UP_ARROW)&&player.index!==null){
+      player.distance+=10;
+      player.update();
+    }
+  }
 }
